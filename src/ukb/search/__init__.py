4e22fe2d225efd29c97cc5b5772c4e8842c10eea
yashumani <51677943+yashumani@users.noreply.github.com>
2026-08-13 12:59:55 -0400

    Export search index interfaces

--- src/ukb/search/__init__.py
+++ src/ukb/search/__init__.py
@@ -1,1 +1,21 @@
-"""Search package."""
+from ukb.search.base import (
+    SearchDocument,
+    SearchHit,
+    SearchIndex,
+    SearchIndexStatus,
+    SearchRequest,
+    SearchResponse,
+    SearchResult,
+)
+from ukb.search.factory import build_search_index
+
+__all__ = [
+    "SearchDocument",
+    "SearchHit",
+    "SearchIndex",
+    "SearchIndexStatus",
+    "SearchRequest",
+    "SearchResponse",
+    "SearchResult",
+    "build_search_index",
+]
