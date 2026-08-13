d7d9cb794b81124b6818220140282df4d689af28
yashumani <51677943+yashumani@users.noreply.github.com>
2026-08-13 13:04:41 -0400

    Initialize source connector package

---
+++ src/ukb/connectors/__init__.py
@@ -0,0 +1,15 @@
+from ukb.connectors.web import (
+    WebCaptureRequest,
+    WebCaptureResponse,
+    WebConnectorError,
+    WebConnectorStatus,
+    WebKnowledgeConnector,
+)
+
+__all__ = [
+    "WebCaptureRequest",
+    "WebCaptureResponse",
+    "WebConnectorError",
+    "WebConnectorStatus",
+    "WebKnowledgeConnector",
+]
